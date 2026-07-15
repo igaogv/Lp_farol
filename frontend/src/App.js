72122e2cba55644be7a235ab107fadf49ba939a3
Igor Mendes <igorbelardini@gmail.com>
2026-07-15 09:48:14 -0300

    Instala Meta Pixel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,47 @@
-import "@/App.css";
+import "@App.css";
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "@/pages/LandingPage";
 
 function App() {
+
+  useEffect(() => {
+
+    !(function(f,b,e,v,n,t,s){
+      if(f.fbq) return;
+
+      n = f.fbq = function(){
+        n.callMethod
+          ? n.callMethod.apply(n, arguments)
+          : n.queue.push(arguments);
+      };
+
+      if(!f._fbq) f._fbq = n;
+
+      n.push = n;
+      n.loaded = true;
+      n.version = "2.0";
+      n.queue = [];
+
+      t = b.createElement(e);
+      t.async = true;
+      t.src = v;
+
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+
+    })(
+      window,
+      document,
+      "script",
+      "https://connect.facebook.net/en_US/fbevents.js"
+    );
+
+    window.fbq("init", "9775540749175790");
+    window.fbq("track", "PageView");
+
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
